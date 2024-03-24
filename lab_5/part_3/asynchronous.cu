b664d88b0d7b873488cabcad885e153187abc249
Dries Van de Velde <driesvandevelde@icloud.com>
2024-03-24 17:49:05 +0100

    GPU_extended functions added with corresponding timing results ;

---
+++ lab_5/part_3/asynchronous.cu
@@ -0,0 +1,270 @@
+#include <chrono>
+#include <cstdio>
+#include <stdexcept>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#define ARR_SIZE_MAX 100000000
+#define ARR_SIZE_MIN 10
+#define ARR_SIZE_STEP 10
+#define NUM_RUNS 2
+
+/**
+ * CUDA device code for performing element-wise summation operation on array
+ */
+__global__ void summation(int *x, int *y, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                int temp = 0;
+                for (size_t i = 0; i < 100000000; i++)
+                {
+                    temp += i;
+                }
+                x[idx] *= x[idx + numElements / 2] + temp;
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *y = x[0];
+    }
+}
+
+/**
+ * CUDA device code for performing element-wise multiplication on array
+ */
+__global__ void multiplication(int *x, int *y, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                int temp = 0;
+                for (size_t i = 0; i < 100000000; i++)
+                {
+                    temp += i;
+                }
+                x[idx] *= x[idx + numElements / 2] + temp;
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *y = x[0];
+    }
+}
+
+/**
+ * CUDA device code for returning the smallest element from array
+ */
+__global__ void minimum(int *input, int *result, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                if (input[idx] > input[idx + numElements / 2])
+                {
+                    int temp = 0;
+                    for (size_t i = 0; i < 100000000; i++)
+                    {
+                        temp += i;
+                    }
+                    input[idx] *= input[idx + numElements / 2] + temp;
+                }
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *result = input[0];
+    }
+}
+
+/**
+ * CUDA device code for returning the largest element from array
+ */
+__global__ void maximum(int *input, int *result, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                if (input[idx] < input[idx + numElements / 2])
+                {
+                    int temp = 0;
+                    for (size_t i = 0; i < 100000000; i++)
+                    {
+                        temp += i;
+                    }
+                    input[idx] *= input[idx + numElements / 2] + temp;
+                }
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *result = input[0];
+    }
+}
+
+
+int main(void)
+{
+    std::ofstream outputFile("./../timings/asynchronous_cuda_results_2.csv");
+    if (!outputFile.is_open())
+    {
+        std::cerr << "Failed to open output file." << std::endl;
+        return 1;
+    }
+
+    outputFile << "Array Size,Execution time (µs)" << std::endl;
+
+    for (int arr_size = 10; arr_size <= ARR_SIZE_MAX; arr_size *= 10)
+    {
+        // Calculate number of blocks and threads
+        int numThreads = 1024;
+        int numBlocks = (arr_size + numThreads - 1) / numThreads;
+
+        if (numThreads > 1024)
+        {
+            throw std::runtime_error("Array size is too large for current implementation. Maximum array size is 1024 elements.");
+        }
+
+        // Init timers
+        float total_time = 0, total_data_prep_time = 0, total_gpu_exec_time = 0;
+
+        // Perform the operations with streams
+        for (int run = 0; run < NUM_RUNS; ++run)
+        {
+            // Start the timer for the operations
+            auto start_operations = std::chrono::high_resolution_clock::now();
+
+            // Create CUDA streams
+            cudaStream_t stream1, stream2, stream3, stream4;
+            cudaStreamCreate(&stream1);
+            cudaStreamCreate(&stream2);
+            cudaStreamCreate(&stream3);
+            cudaStreamCreate(&stream4);
+
+            // Copy data to device asynchronously
+
+            std::vector<int> arr_1(arr_size);
+            for (int i = 0; i < arr_size; i++)
+            {
+                arr_1[i] = rand() % 1000;
+            }
+            int *d_arr_1;
+            cudaMalloc((void **)&d_arr_1, arr_size * sizeof(int));
+            int *d_out_1;
+            cudaMalloc((void **)&d_out_1, sizeof(int));
+
+            cudaMemcpyAsync(d_arr_1, arr_1.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice, stream1);
+            summation<<<numBlocks, numThreads, 0, stream1>>>(d_arr_1, d_out_1, arr_size);
+
+            std::vector<int> arr_2(arr_size);
+            for (int i = 0; i < arr_size; i++)
+            {
+                arr_2[i] = rand() % 1000;
+            }
+            int *d_arr_2;
+            cudaMalloc((void **)&d_arr_2, arr_size * sizeof(int));
+            int *d_out_2;
+            cudaMalloc((void **)&d_out_2, sizeof(int));
+
+            cudaMemcpyAsync(d_arr_2, arr_2.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice, stream2);
+            multiplication<<<numBlocks, numThreads, 0, stream2>>>(d_arr_2, d_out_2, arr_size);
+
+            std::vector<int> arr_3(arr_size);
+            for (int i = 0; i < arr_size; i++)
+            {
+                arr_3[i] = rand() % 1000;
+            }
+            int *d_arr_3;
+            cudaMalloc((void **)&d_arr_3, arr_size * sizeof(int));
+            int *d_out_3;
+            cudaMalloc((void **)&d_out_3, sizeof(int));
+
+            cudaMemcpyAsync(d_arr_3, arr_3.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice, stream3);
+            maximum<<<numBlocks, numThreads, 0, stream3>>>(d_arr_3, d_out_3, arr_size);
+
+            std::vector<int> arr_4(arr_size);
+            for (int i = 0; i < arr_size; i++)
+            {
+                arr_4[i] = rand() % 1000;
+            }
+            int *d_arr_4;
+            cudaMalloc((void **)&d_arr_4, arr_size * sizeof(int));
+            int *d_out_4;
+            cudaMalloc((void **)&d_out_4, sizeof(int));
+
+            cudaMemcpyAsync(d_arr_4, arr_4.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice, stream4);
+            minimum<<<numBlocks, numThreads, 0, stream4>>>(d_arr_4, d_out_4, arr_size);
+
+            // Synchronize streams
+            cudaStreamSynchronize(stream1);
+            cudaStreamSynchronize(stream2);
+            cudaStreamSynchronize(stream3);
+            cudaStreamSynchronize(stream4);
+
+            // Destroy streams after use
+            cudaStreamDestroy(stream1);
+            cudaStreamDestroy(stream2);
+            cudaStreamDestroy(stream3);
+            cudaStreamDestroy(stream4);
+
+            auto end_operations = std::chrono::high_resolution_clock::now();
+            total_time += std::chrono::duration_cast<std::chrono::microseconds>(end_operations - start_operations).count();
+
+            // Free device memory
+            cudaFree(d_arr_1);
+            cudaFree(d_out_1);
+            cudaFree(d_arr_2);
+            cudaFree(d_out_2);
+            cudaFree(d_arr_3);
+            cudaFree(d_out_3);
+            cudaFree(d_arr_4);
+            cudaFree(d_out_4);
+        }
+
+        // Calculate the average execution time for each operation
+        float average_time_operations = total_time / NUM_RUNS;
+
+        // Write the results to the CSV file
+        outputFile << arr_size << ","
+                   << average_time_operations << std::endl;
+    }
+
+    // Close the output file
+    outputFile.close();
+
+    return 0;
+}
---
+++ lab_5/part_3/synchronous.cu
@@ -0,0 +1,266 @@
+#include <chrono>
+#include <cstdio>
+#include <stdexcept>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#define ARR_SIZE_MAX 100000000
+#define ARR_SIZE_MIN 10
+#define ARR_SIZE_STEP 10
+#define NUM_ITERATIONS 2
+
+/**
+ * CUDA device code for performing element-wise summation operation on array
+ */
+__global__ void summation(int *x, int *y, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                int temp = 0;
+                for (size_t i = 0; i < 100000000; i++)
+                {
+                    temp += i;
+                }
+                x[idx] *= x[idx + numElements / 2] + temp;
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *y = x[0];
+    }
+}
+
+/**
+ * CUDA device code for performing element-wise multiplication on array
+ */
+__global__ void multiplication(int *x, int *y, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                int temp = 0;
+                for (size_t i = 0; i < 100000000; i++)
+                {
+                    temp += i;
+                }
+                x[idx] *= x[idx + numElements / 2] + temp;
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *y = x[0];
+    }
+}
+
+/**
+ * CUDA device code for returning the smallest element from array
+ */
+__global__ void minimum(int *input, int *result, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                if (input[idx] > input[idx + numElements / 2])
+                {
+                    int temp = 0;
+                    for (size_t i = 0; i < 100000000; i++)
+                    {
+                        temp += i;
+                    }
+                    input[idx] *= input[idx + numElements / 2] + temp;
+                }
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *result = input[0];
+    }
+}
+
+/**
+ * CUDA device code for returning the largest element from array
+ */
+__global__ void maximum(int *input, int *result, int numElements)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < numElements)
+    {
+        while (numElements > 1)
+        {
+            if (idx < numElements / 2)
+            {
+                if (input[idx] < input[idx + numElements / 2])
+                {
+                    int temp = 0;
+                    for (size_t i = 0; i < 100000000; i++)
+                    {
+                        temp += i;
+                    }
+                    input[idx] *= input[idx + numElements / 2] + temp;
+                }
+            }
+            __syncthreads();
+            numElements /= 2;
+        }
+    }
+    if (idx == 0)
+    {
+        *result = input[0];
+    }
+}
+
+int main(void)
+{
+    // Open CSV file for writing
+    std::ofstream csv_file("./../timings/synchronous_cuda_results_2.csv");
+    csv_file << "Array size, GPU execution time (µs), Data Prep time (µs), Data malloc time (µs), Total Execution time (µs)";
+
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Warm up the GPU
+    int *d_dummy;
+    cudaMalloc((void **)&d_dummy, sizeof(int));
+    summation<<<1, 1>>>(nullptr, d_dummy, 1);
+    multiplication<<<1, 1>>>(nullptr, d_dummy, 1);
+    minimum<<<1, 1>>>(nullptr, d_dummy, 1);
+    maximum<<<1, 1>>>(nullptr, d_dummy, 1);
+    cudaFree(d_dummy);
+
+    // Loop over array sizes from 10 to ARR_SIZE_MAX
+    for (int arr_size = 10; arr_size <= ARR_SIZE_MAX; arr_size *= 10)
+    {
+        // Calculate number of blocks and threads
+        int numThreads = (arr_size + 1) / 2;
+        int numThreadsMax = 1024;
+        numThreads = numThreads > numThreadsMax ? numThreadsMax : numThreads;
+        int numBlocks = (arr_size + numThreads - 1) / numThreads;
+
+        if (numThreads > 1024)
+        {
+            throw std::runtime_error("Array size is too large for current implementation. Maximum array size is 1024 elements.");
+        }
+
+        // Perform benchmarking for each operation
+        float summation_time = 0, multiplication_time = 0, minimum_time = 0, maximum_time = 0, data_prep_time = 0, data_malloc_time = 0;
+        for (int iter = 0; iter < NUM_ITERATIONS; iter++)
+        {
+            auto start_data_prep = std::chrono::high_resolution_clock::now();
+
+            // Allocate memory for host vectors
+            std::vector<int> arr_1(arr_size);
+            std::vector<int> arr_2(arr_size);
+            std::vector<int> arr_3(arr_size);
+            std::vector<int> arr_4(arr_size);
+
+            // Fill host vectors with random numbers
+            for (int i = 0; i < arr_size; i++)
+            {
+                arr_1[i] = rand() % 1000;
+                arr_2[i] = rand() % 1000;
+                arr_3[i] = rand() % 1000;
+                arr_4[i] = rand() % 1000;
+            }
+
+            auto start_data_malloc = std::chrono::high_resolution_clock::now();
+
+            // Allocate memory on the device
+            int *d_arr_1, *d_arr_2, *d_arr_3, *d_arr_4;
+            cudaMalloc((void **)&d_arr_1, arr_size * sizeof(int));
+            cudaMalloc((void **)&d_arr_2, arr_size * sizeof(int));
+            cudaMalloc((void **)&d_arr_3, arr_size * sizeof(int));
+            cudaMalloc((void **)&d_arr_4, arr_size * sizeof(int));
+
+            auto end_data_malloc = std::chrono::high_resolution_clock::now();
+            data_malloc_time += std::chrono::duration_cast<std::chrono::microseconds>(end_data_malloc - start_data_malloc).count();
+
+            auto end_data_prep = std::chrono::high_resolution_clock::now();
+            data_prep_time += std::chrono::duration_cast<std::chrono::microseconds>(end_data_prep - start_data_prep).count();
+
+            // Copy host vectors to device
+            cudaMemcpy(d_arr_1, arr_1.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_arr_2, arr_2.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_arr_3, arr_3.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_arr_4, arr_4.data(), arr_size * sizeof(int), cudaMemcpyHostToDevice);
+            cudaDeviceSynchronize();
+
+            // Start the timer for each operation
+            auto start_summation = std::chrono::high_resolution_clock::now();
+            summation<<<numBlocks, numThreads>>>(d_arr_1, nullptr, arr_size);
+            cudaDeviceSynchronize();
+            auto end_summation = std::chrono::high_resolution_clock::now();
+            summation_time += std::chrono::duration_cast<std::chrono::microseconds>(end_summation - start_summation).count();
+
+            auto start_multiplication = std::chrono::high_resolution_clock::now();
+            multiplication<<<numBlocks, numThreads>>>(d_arr_2, nullptr, arr_size);
+            cudaDeviceSynchronize();
+            auto end_multiplication = std::chrono::high_resolution_clock::now();
+            multiplication_time += std::chrono::duration_cast<std::chrono::microseconds>(end_multiplication - start_multiplication).count();
+
+            auto start_minimum = std::chrono::high_resolution_clock::now();
+            minimum<<<numBlocks, numThreads>>>(d_arr_3, nullptr, arr_size);
+            cudaDeviceSynchronize();
+            auto end_minimum = std::chrono::high_resolution_clock::now();
+            minimum_time += std::chrono::duration_cast<std::chrono::microseconds>(end_minimum - start_minimum).count();
+
+            auto start_maximum = std::chrono::high_resolution_clock::now();
+            maximum<<<numBlocks, numThreads>>>(d_arr_4, nullptr, arr_size);
+            cudaDeviceSynchronize();
+            auto end_maximum = std::chrono::high_resolution_clock::now();
+            maximum_time += std::chrono::duration_cast<std::chrono::microseconds>(end_maximum - start_maximum).count();
+
+            // Free device memory
+            cudaFree(d_arr_1);
+            cudaFree(d_arr_2);
+            cudaFree(d_arr_3);
+            cudaFree(d_arr_4);
+        }
+
+        // Compute average time for each operation
+        summation_time /= NUM_ITERATIONS;
+        multiplication_time /= NUM_ITERATIONS;
+        minimum_time /= NUM_ITERATIONS;
+        maximum_time /= NUM_ITERATIONS;
+        data_prep_time /= NUM_ITERATIONS;
+        data_malloc_time /= NUM_ITERATIONS;
+
+        auto execution_time = summation_time + multiplication_time + minimum_time + maximum_time;
+        auto total_time = execution_time + data_prep_time + data_malloc_time;
+
+        // Write results to CSV
+        csv_file << "\n"
+                 << arr_size << ", " << execution_time << ", " << data_prep_time << ", " << data_malloc_time << ", " << total_time;
+    }
+
+    // Close CSV file
+    csv_file.close();
+
+    return 0;
+}
